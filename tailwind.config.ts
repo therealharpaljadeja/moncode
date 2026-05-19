03395f3bd1a6e0865941b1c48d7e4d0417bde921
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2026-05-19 12:21:36 +0530

    feat: add prompt ui components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,11 +73,39 @@ const config: Config = {
   				to: {
   					height: '0'
   				}
+  			},
+  			'collapsible-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-collapsible-content-height)'
+  				}
+  			},
+  			'collapsible-up': {
+  				from: {
+  					height: 'var(--radix-collapsible-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
   			}
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
+  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			'collapsible-down': 'collapsible-down 0.2s ease-out',
+  			'collapsible-up': 'collapsible-up 0.2s ease-out'
+  		}
+  	},
+  	keyframes: {
+  		shimmer: {
+  			'0%': {
+  				backgroundPosition: '200% 50%'
+  			},
+  			'100%': {
+  				backgroundPosition: '-200% 50%'
+  			}
   		}
   	}
   },
